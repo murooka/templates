f09def89db91ebccc3ec9b4b019281d8c98d9a8e
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2016-07-11 00:09:43 +0900

    Add lint to web-vue

---
+++ web-vue/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": ["eslint:recommended"],
+    "plugins": [],
+    "parserOptions": {},
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+    },
+    "globals": {},
+    "rules": {},
+}
